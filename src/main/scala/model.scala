dd4d38167c459bb7d84753da2cc6b0409685e127
AlexsandrY <alexsandrvladimirovich@gmail.com>
2013-04-05 16:35:43 +0400

    Red

--- src/main/scala/model.scala
+++ src/main/scala/model.scala
@@ -2,19 +2,55 @@ package org.adressbook
 
 import pro.savant.circumflex._, orm._
 import java.util.Date
+import org.eclipse.jetty.server.Authentication.User
 
-/**class Contact
+class Contact
     extends Record[Long, Contact]
     with IdentityGenerator[Long, Contact] {
 
-
-
-
+  def PRIMARY_KEY: ValueHolder[Long, Contact] = id
+  def relation: Relation[Long, Contact] = Contact
 
   val id = "id".BIGINT.NOT_NULL.AUTO_INCREMENT
   val firstName = "firstName".HTML.NOT_NULL
   val secondName = "secondName".HTML.NOT_NULL
+  val surName = "surName".HTML.NOT_NULL
+  val post = "post".HTML.NOT_NULL("")
+  val company = "company".HTML.NOT_NULL("")
+  val address = "address".HTML.NOT_NULL("")
+  val dateofBirth = "dateofBirth".DATE(new Date)
+}
+
+object Contact extends Contact with Table[Long, Contact] {
 
 
-}**/
+  validation
+      .notEmpty(_.firstName)
+      .notEmpty(_.secondName)
+      .notEmpty(_.surName)
+}
+
+class Phone
+    extends Record[Long, Phone]
+    with IdentityGenerator[Long, Phone] {
+
+  def PRIMARY_KEY: ValueHolder[Long, Phone] = ???
+
+  def relation: Relation[Long, Phone] = ???
+
+  val id = "id".BIGINT.NOT_NULL.AUTO_INCREMENT
+  val phoneType = "phoneType".HTML.NOT_NULL
+  val telephoneNumber = "telephoneNumber".HTML.NOT_NULL
+}
+
+object Phone extends Phone with Table[Long, Phone] {
+
+
+  validation
+      .notEmpty(_.)
+
+
+}
+
+
 
